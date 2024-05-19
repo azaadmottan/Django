6bc8e37b54e9ccf2b3a09139afacaf292a631890
Azaad Mottan <azaadmottan2022@gmail.com>
2024-05-19 13:42:45 +0530

    Directory exploration and create templates & static folder and learn setup of these folders.

--- django_01/firstDjango/firstDjango/views.py
+++ django_01/firstDjango/firstDjango/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(req):
-    return HttpResponse("Hello, User.<br/>Welcome to our website !")
+    # return HttpResponse("Hello, User.<br/>Welcome to our website !")
+    return render(req, 'pages/index.html')
 
 def about(req):
-    return HttpResponse("This is about page")
+    # return HttpResponse("This is about page")
+    return render(req, 'pages/about.html')
 
 def contact(req):
-    return HttpResponse("This is contact page")
+    # return HttpResponse("This is contact page")
+    return render(req, 'pages/contact.html')
